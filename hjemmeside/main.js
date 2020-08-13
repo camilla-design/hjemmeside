a81f6d52db7f032de40f7aac914a873acc1219ff
camilla-design <56583550+camilla-design@users.noreply.github.com>
2020-08-13 14:16:34 +0200

    Add files via upload

---
+++ hjemmeside/main.js
@@ -0,0 +1,7 @@
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("navigation").classList.toggle("change");
+	
+	document.getElementById("menu-background").classList.toggle("change-bg");
+}
+
